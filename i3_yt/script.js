3768e4d830631373b69056ec8a7fb69ee5aaa1dc
Evan <obrienem@rose-hulman.edu>
2022-07-11 01:45:33 -0400

    i3_yt and scrollbar_block

---
+++ i3_yt/script.js
@@ -0,0 +1,38 @@
+var vidCont = document.querySelector(".html5-video-container");
+var hideDone = false;
+var able_id;
+var hide_id;
+
+function hideable() {
+	hideDone = false;
+}
+
+function killTime(timeId) {
+	clearTimeout(timeId);
+}
+
+function hide() {
+	vidCont.style.cursor = 'none';
+	hideDone = true;
+};
+
+function hideTime() {
+	killTime(hide_id);
+	killTime(able_id);
+	if(!hideDone) { 
+		vidCont.style.cursor = 'auto';
+	}
+	hide_id = setTimeout(hide, 180);
+	able_id = setTimeout(hideable, 24);
+};		
+
+function unhide() {
+	vidCont.style.cursor = 'auto';
+	hideDone = false;
+}
+
+//vidCont.addEventListener('mouseover', hideTime);
+vidCont.addEventListener('mouseout', unhide);
+vidCont.addEventListener('mousemove', hideTime);
+
+
